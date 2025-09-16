6b38533bb6041d7ab689a3f1e6927691aab20f60
Back <raphael.back1@gmail.com>
2025-09-16 13:49:58 +0200

    update

--- src/CarMain.ts
+++ src/CarMain.ts
@@ -1,10 +1,27 @@
-// main.ts
-
 import { Car } from "./Car";
 
-const myCar = new Car("A3", "Audi", "Noir", 2022);
+const audi = new Car("A3", "Audi", "Noir", 2022);
+const bmw = new Car("X5", "BMW", "Blanc", 2021);
+const toyota = new Car("Corolla", "Toyota", "Bleu", 2020);
+const tesla = new Car("Model 3", "Tesla", "Rouge", 2023);
 
-myCar.start();
-myCar.accelerate(50);
-myCar.accelerate(30);
-myCar.stop();
+console.log("\n=== Audi ===");
+audi.start();
+audi.accelerate(40);
+audi.stop();
+
+console.log("\n=== BMW ===");
+bmw.start();
+bmw.accelerate(60);
+bmw.accelerate(20);
+bmw.stop();
+
+console.log("\n=== Toyota ===");
+toyota.start();
+toyota.accelerate(30);
+toyota.stop();
+
+console.log("\n=== Tesla ===");
+tesla.start();
+tesla.accelerate(100);
+tesla.stop();
